1d396a56a6a484c8c2d282943cc74744c55514a3
vo huu nhan <vohuunhan1310@gmail.com>
2024-07-13 03:56:18 +0700

    deploy

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const { releyTransaction }=require("./src");
+const express = require('express');
+const app = express();
+var cors = require('cors');
+app.use(cors())
+app.use(express.json());
+
+app.post("/relay",async(req,res)=>{
+    const body = req.body;
+    const rs = await releyTransaction(JSON.parse(body.delegate));
+    const data = rs.result;
+    console.log("reuslt: ",rs)
+    res.json(data);
+})
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
